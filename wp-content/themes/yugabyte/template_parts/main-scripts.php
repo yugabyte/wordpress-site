3b22035f878133e5799c1db42cb5f8c95746effe
Andrew Cai <andrew@yugabyte.com>
2021-04-29 01:08:28 -0700

    Add handling for fireside chat url to send to heroku server

--- wp-content/themes/yugabyte/template_parts/main-scripts.php
+++ wp-content/themes/yugabyte/template_parts/main-scripts.php
@@ -249,6 +249,8 @@ window.onload = function () {
 					url = `${SERVICE_URL}/multi-cloud-white-paper-jan-2021/download`;
 				} else if (window.location.pathname.match(/dzone-data-persistence-report-2021/)) {
 					url = `${SERVICE_URL}/dzone-data-persistence-2021/download`;
+				} else if (window.location.pathname.match(/catalysts-of-change-retail-fireside-chat-kohls/)) {
+					url= `${SERVICE_URL}/retail-fireside-chat/register`;
 				}
 				if (url) {
 					fetch(url, {
