a8863502e0a83cd09bcc3f5e8c82dd1cd6b7e3b7
bigbritches <willsbigbritches@gmail.com>
2021-06-18 22:07:05 -0400

    per request, removing excerpt logic on Events, just rendering full the_content instead

--- wp-content/themes/yugabyte/alm_templates/template_2.php
+++ wp-content/themes/yugabyte/alm_templates/template_2.php
@@ -46,11 +46,14 @@
     }
     $link_tar = ( $ext_url ) ? '_blank' : '_self';
     
-    if( has_excerpt() ) {
+    /*if( has_excerpt() ) {
         $ev_excerpt = get_the_excerpt();
     } else {
         $ev_excerpt = substr( strip_tags( strip_shortcodes(get_the_content()) ), 0, 200 );
-    }
+    }*/
+    
+    //NOT USING EXCERPTS, JUST RENDER THE FULL CONTENT
+    $ev_excerpt = get_the_content();
     
     //IMAGE
     $has_img = ( $conf_img ) ? '' : 'full';
@@ -123,7 +126,7 @@
         }
         
         echo '</p>';
-        echo '<p>'.$ev_excerpt.'</p>';
+        echo $ev_excerpt;
         echo '<a href="'.$link.'" class="btn sq small nomargin" target="'.$link_tar.'">Read more</a>';
     echo '</div></div>';
     ?>
